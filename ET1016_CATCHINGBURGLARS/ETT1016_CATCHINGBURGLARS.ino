03f9f2edfce6489f6edf7ee82ff67b11f6f9b1ba
EvanM2008 <evanmathew.25@ichat.sp.edu.sg>
2025-07-31 14:54:56 +0800

    Update ETT1016_CATCHINGBURGLARS.ino

--- ET1016_CATCHINGBURGLARS/ETT1016_CATCHINGBURGLARS.ino
+++ ET1016_CATCHINGBURGLARS/ETT1016_CATCHINGBURGLARS.ino
@@ -4,9 +4,19 @@
 // DCEP/FT/1A/09
 // BURGLAR ALARM 
 
+void setup() {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+}
 
-
-
+void loop() {
+  digitalWrite(LED_RED, HIGH);
+  delay(500);
+  digitalWrite(LED_RED, LOW);
+  delay(500);
+}
 
 
 
