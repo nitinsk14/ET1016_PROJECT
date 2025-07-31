165b845450e2c7257bdeb74aa9b7a5429af09214
nitinsk14 <Hainitinssk@gmail.com>
2025-07-31 15:00:50 +0800

    Update ETT1016_CATCHINGBURGLARS.ino

--- ET1016_CATCHINGBURGLARS/ETT1016_CATCHINGBURGLARS.ino
+++ ET1016_CATCHINGBURGLARS/ETT1016_CATCHINGBURGLARS.ino
@@ -17,16 +17,3 @@ void loop() {
   digitalWrite(LED_RED, LOW);
   delay(500);
 }
-
-
-
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
