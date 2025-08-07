c702abc051ebd7ac9d0482553cc41b4a50e40cda
nitinsk14 <Hainitinssk@gmail.com>
2025-08-07 15:50:49 +0800

    Update ETT1016_CATCHINGBURGLARS.ino

--- ET1016_CATCHINGBURGLARS/ETT1016_CATCHINGBURGLARS.ino
+++ ET1016_CATCHINGBURGLARS/ETT1016_CATCHINGBURGLARS.ino
@@ -1,8 +1,8 @@
+// BURGLAR ALARM 
 // CATCHINGBURGLARS
 // NITIN & EVAN 
 // P2517704 & P2517478
 // DCEP/FT/1A/09
-// BURGLAR ALARM
 #include <math.h>
 #include <Wire.h>
 
@@ -21,49 +21,7 @@
 PassiveBuzzer buz(BUZZER);
 LightSensor lightsensor(LIGHTSENSOR_PIN);
 
-// Notes for melody
-#define NOTE_L1  262
-#define NOTE_L2  294
-#define NOTE_L3  330
-#define NOTE_L4  349
-#define NOTE_L5  392
-#define NOTE_L6  440
-#define NOTE_L7  494
-#define NOTE_M1  523
-#define NOTE_M2  587
-#define NOTE_M3  659
 #define NOTE_M4  698
-#define NOTE_M5  784
-#define NOTE_M6  880
-#define NOTE_M7  988
-#define NOTE_H1  1046
-#define NOTE_H2  1175
-#define NOTE_H3  1318
-#define NOTE_H4  1397
-#define NOTE_H5  1568
-#define NOTE_H6  1760
-#define NOTE_H7  1976
-
-const static int doubleTiger[] = { 
-  NOTE_M1, NOTE_M2, NOTE_M3, NOTE_M1, 
-  NOTE_M1, NOTE_M2, NOTE_M3, NOTE_M1, 
-  NOTE_M3, NOTE_M4, NOTE_M5, 
-  NOTE_M3, NOTE_M4, NOTE_M5, 
-  NOTE_M5, NOTE_M6, NOTE_M5, NOTE_M4, NOTE_M3, NOTE_M1, 
-  NOTE_M5, NOTE_M6, NOTE_M5, NOTE_M4, NOTE_M3, NOTE_M1, 
-  NOTE_M2, NOTE_L5, NOTE_M1, 0, 
-  NOTE_M2, NOTE_L5, NOTE_M1, 0
-};
-
-const static int tempo[] = { 
-  2, 2, 2, 2, 
-  2, 2, 2, 2, 
-  2, 2, 1, 
-  2, 2, 1,  
-  2, 2, 2, 2, 1, 1,
-  2, 2, 2, 2, 1, 1,
-  1, 1, 1, 1, 1, 1
-};
 
 int armed = 0;
 unsigned long lastButtonTime = 0;
@@ -115,15 +73,6 @@ int isIntrusionDetected() {
   else return 0;
 }
 
-// Play alarm melody
-void sing() {
-  int size = sizeof(doubleTiger) / sizeof(int);
-  for (int i = 0; i < size; i++) {
-    int noteDuration = 500 / tempo[i];
-    buz.playTone(doubleTiger[i], noteDuration);
-    delay(noteDuration * 0.3); // spacing between notes
-  }
-}
 
 void setup() {
   pinMode(BTN, INPUT_PULLUP);
@@ -146,7 +95,7 @@ void loop() {
     showArmed();
     if (isIntrusionDetected()) {
       showAlarm();
-      sing();
+      buz.playTone(698,10000);
     }
   } else {
     showDisarmed();
